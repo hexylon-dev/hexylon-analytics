bb53879ea6f20f61bb04944435ebd1094016f874
Vivek151003 <vivekhasubhaidafda@gmail.com>
2024-12-10 10:48:25 +0530

    new things added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,10 +7,11 @@ import IndustriesSection from "../components/IndustriesSection2";
 import OurWorkSection from "../components/OurWork4";
 import CommitmentSection from "../components/OurCommit1";
 import Footer from "../components/Footer";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
 
+  const navigate = useNavigate();
   const handleNavigateToContactPage = () => {
     window.location.href = "/contact-us";
   };
@@ -32,17 +33,8 @@ function Home() {
       <IndustriesSection />
       <OurWorkSection />
       <CommitmentSection handleNavigateToContactPage={handleNavigateToContactPage} />
-      <Footer handleNavigateToContactPage={handleNavigateToContactPage} />
+      {/* <Footer handleNavigateToContactPage={handleNavigateToContactPage} /> */}
 
-      {/* Career Navigation Button */}
-      <div className="text-center py-8">
-        <button
-          onClick={handleNavigateToCareersPage}
-          className="bg-[#FF6600] text-white py-3 px-8 rounded-full hover:bg-[#FF8533] transition duration-300"
-        >
-          Explore Careers
-        </button>
-      </div>
     </>
   );
 }
