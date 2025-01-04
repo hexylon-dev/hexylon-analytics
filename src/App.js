5c248afe47376b1edf1daba8275be7b5f551b293
Vivek151003 <vivekhasubhaidafda@gmail.com>
2025-01-04 15:15:53 +0530

    conflictResolved By v

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ContactUs from "./pages/ContactUs4";
 import Navbar from "./components/Navbar1";
 import Footer from "./components/Footer";
 import CareerPage from './components/CareerPage';
-import BlogDetail from "./pages/BlogDetail";
 import ApplyForm from "./components/ApplyForm";
 import NotificationWrapper from "./components/notifiction/notifiction";
 import ServicePageV from "./components/ServicePageV";
@@ -26,8 +25,8 @@ function App() {
             <main className="container mx-auto px-4 py-12">
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16">
                 {resources.map((resource) => (
-                  <ResourceCard
-                    key={resource.id}
+                  <ResourceCard 
+                    key={resource.id} 
                     {...resource}
                   />
                 ))}
@@ -40,15 +39,7 @@ function App() {
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/careers" element={<CareerPage />} />
         <Route path="/apply-form/:job_id" element={<ApplyForm />} />
-        <Route path="/services" element={
-          <div className=" min-h-screen">
-            <ServicePageV
-              title="AI and ML <br /> Service"
-              description="Anticipating customers needs to improve demand planning and streamline inventory management."
-              metaDescription="Leverage AI and Machine Learning solutions to transform your business operations, enhance customer experience, and drive data-driven decisions."
-            />
-          </div>
-        } />
+        <Route path="/services" element={<ServicePageV title="AI and ML <br /> Service" description="Anticipating customers needs to improve demand planning and streamline inventory management." />} />
       </Routes>
       <Footer />
     </Router>
